f761382401e2ca08650b29c39bb6664366e00ebd
Baalamurgan <baalamurgan2307@gmail.com>
2021-10-10 00:03:31 +0530

    feat: Test Support 🤘🤘

--- main_test.go
+++ main_test.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
+	"github.com/baalamurgan/crud-golang/pkg/config"
+	"github.com/baalamurgan/crud-golang/pkg/models"
 	"github.com/baalamurgan/crud-golang/pkg/routes"
+	"github.com/baalamurgan/crud-golang/pkg/utils"
+	guuid "github.com/google/uuid"
 	"github.com/gorilla/mux"
 )
 
 func Test_getUser(t *testing.T) {
+	config.Connect()
 	r := mux.NewRouter()
 	routes.RegisterWeatherRoutes(r)
 
@@ -17,7 +25,20 @@ func Test_getUser(t *testing.T) {
 
 	ts := httptest.NewServer(r)
 	defer ts.Close()
-	res, err := http.Get(ts.URL + "/user")
+
+	newUser := models.User{
+		ID:        guuid.New().String(),
+		Name:      "test",
+		Email:     "test@gamil.com",
+		Password:  utils.HashPassword("test"),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	postBody, _ := json.Marshal(newUser)
+	responseBody := bytes.NewBuffer(postBody)
+
+	res, err := http.Post(ts.URL+"/user", "application/json", responseBody)
 
 	if err != nil {
 		t.Errorf("Expected nil, received %s", err.Error())
